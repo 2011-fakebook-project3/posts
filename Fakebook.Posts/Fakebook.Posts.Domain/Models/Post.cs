6a82eb249b5408cf188ea79cdd3210392e58c92f
Matthew Goodman <mgoodman934@gmail.com>
2020-12-29 18:17:02 -0500

    set up domain class properties

--- Fakebook.Posts/Fakebook.Posts.Domain/Models/Post.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/Models/Post.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Fakebook.Posts.Domain {
-    class Post {
+    public class Post {
+
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Picture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+        
+        public ICollection<Comment> Comments { get; set; }
+
+        public Post() {
+            Comments = new List<Comment>();
+        }
     }
 }
