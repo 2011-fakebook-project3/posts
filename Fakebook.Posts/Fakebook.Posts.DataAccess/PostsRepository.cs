e935f426e9dc47710e8c9fb0bdf6bd6703276b64
Alex Soto <alex.soto0624@gmail.com>
2020-12-30 13:27:36 -0800

    Started Post Repository Testing

--- Fakebook.Posts/Fakebook.Posts.DataAccess/PostsRepository.cs
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/PostsRepository.cs
@@ -5,8 +5,58 @@ namespace Fakebook.Posts.DataModel
     public class PostsRepository 
     {
         private readonly FakebookPostsContext _context;
-        public PostsRepository(FakebookPostsContext context) {
+        public PostsRepository(FakebookPostsContext context)
+        {
             _context = context;
+        }
+
+        public int Count => throw new System.NotImplementedException();
+
+        public bool IsReadOnly => false;
+
+        public void Add(Fakebook.Posts.Domain.Post item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ValueTask<bool> AddAsync(Fakebook.Posts.Domain.Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(Fakebook.Posts.Domain.Post item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(Fakebook.Posts.Domain.Post[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAsyncEnumerator<Fakebook.Posts.Domain.Post> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<Fakebook.Posts.Domain.Post> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(Fakebook.Posts.Domain.Post item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
---
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/PostRepository Test/PostRepository_CreateTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using FakebookPosts.DataModel;
+using Fakebook.Posts.Domain;
+using Microsoft.Data.Sqlite;
+using Xunit;
+
+namespace Fakebook.Posts.UnitTests.PostRepository_Test
+{
+    public class PostRepository_CreateTest
+    {
+        [Fact]
+        public void CreatePost()
+        {
+            //Arrange
+            using var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<FakebookPostsContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            Domain.Post post = new Domain.Post
+            {
+                
+            };
+
+            bool result;
+
+
+            //Act
+            using (var context = new FakebookPostsContext(options))
+            {
+                context.Database.EnsureCreated();
+                var repo = new PostsRepository(context);
+               
+            }
+
+
+        }
+    }
+}
