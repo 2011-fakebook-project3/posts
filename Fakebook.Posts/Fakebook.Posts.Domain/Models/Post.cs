cae6525c814a65d4e362da21f67a1f5e290e5f7f
nodeJayS <60407405+nodeJayS@users.noreply.github.com>
2020-12-30 09:50:35 -0500

    Update Post.cs
    Removed content from post domain model

--- Fakebook.Posts/Fakebook.Posts.Domain/Models/Post.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/Models/Post.cs
@@ -4,17 +4,5 @@ using System.Text;
 
 namespace Fakebook.Posts.Domain {
     public class Post {
-
-        public int Id { get; set; }
-        public string Content { get; set; }
-        public string Picture { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public int UserId { get; set; }
-        
-        public ICollection<Comment> Comments { get; set; }
-
-        public Post() {
-            Comments = new List<Comment>();
-        }
     }
 }
