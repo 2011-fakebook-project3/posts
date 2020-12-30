bf3caf45b5407919320fdffd7a37ffd06f13473a
harelfishbein <harelfishbein@github.com>
2020-12-30 12:51:23 -0800

    moved the namespace of post to models folder

--- Fakebook.Posts/Fakebook.Posts.Domain/Models/Comment.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/Models/Comment.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 
-namespace Fakebook.Posts.Domain {
+namespace Fakebook.Posts.Domain.Models {
     public class Comment {
         public int Id { get; set; }
         public string Content { get; set; }
