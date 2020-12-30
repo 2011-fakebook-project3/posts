a310c29a03e692e9aef8de9cb0ec80a110d19a64
Matthew Goodman <mgoodman934@gmail.com>
2020-12-30 14:46:54 -0500

    Create posts controller with empty Post method

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Fakebook.Posts.Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,5 +10,14 @@ namespace Fakebook.Posts.RestApi.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class PostsController : ControllerBase {
+        public PostsController() {
+
+        }
+
+        // [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Post(Post postModel) {
+            throw new NotImplementedException();
+        }
     }
 }
