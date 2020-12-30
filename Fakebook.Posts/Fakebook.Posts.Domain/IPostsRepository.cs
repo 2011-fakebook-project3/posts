ced890bb97370886d2ce965e6b98b97d50b56854
harelfishbein <harelfishbein@github.com>
2020-12-30 12:45:02 -0800

    change addAsync to return post

--- Fakebook.Posts/Fakebook.Posts.Domain/IPostsRepository.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/IPostsRepository.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Fakebook.Posts.Domain.Models;
 
 namespace Fakebook.Posts.Domain {
     public interface IPostsRepository : ICollection<Post>, IEnumerable<Post>, IAsyncEnumerable<Post>
     {
-        public ValueTask<bool> AddAsync(Post post);
+        public ValueTask<Post> AddAsync(Post post);
         // public ValueTask<bool> RemoveAsync(Post post);
     }
 }
