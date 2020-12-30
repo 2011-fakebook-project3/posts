a9b59f123012f1e7959954d3f4ebacc1f5c679f1
Matthew Goodman <mgoodman934@gmail.com>
2020-12-30 16:40:39 -0500

    Initial post implementation

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
@@ -1,22 +1,45 @@
-﻿using Fakebook.Posts.Domain;
+﻿using Fakebook.Posts.Domain.Models;
+using Fakebook.Posts.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Fakebook.Posts.RestApi.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class PostsController : ControllerBase {
-        public PostsController() {
 
+        private readonly IPostsRepository _postsRepository;
+
+        public PostsController(IPostsRepository postsRepository) {
+            _postsRepository = postsRepository;
         }
 
-        // [Authorize]
+        [Authorize]
         [HttpPost]
-        public async Task<IActionResult> Post(Post postModel) {
+        public async Task<IActionResult> PostAsync(Post postModel) {
+            // var email = User.FindFirst(ct => ct.Type.Contains("nameidentifier")).Value; // Get user email from session.
+            // verify user creating the post here...
+            Post created;
+            try {
+                created = await _postsRepository.AddAsync(postModel);
+            } catch (ArgumentException e) { // Attempted to create a post with invalid arguments.
+                return BadRequest(e.Message);
+            } catch (DbUpdateException e) { // Attempted to create a post which violated database constraints.
+                return BadRequest(e.Message);
+            }
+
+            return CreatedAtAction(nameof(GetAsync), new { id = created.Id }, created);
+        }
+
+        [HttpGet("{id}")]
+        [ActionName(nameof(GetAsync))]
+        public async Task<IActionResult> GetAsync(int id) {
             throw new NotImplementedException();
         }
     }
