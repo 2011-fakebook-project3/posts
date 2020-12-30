2fa1f6e4c31d033e7409e9d4cc058592f9434258
Alex Soto <alex.soto0624@gmail.com>
2020-12-30 09:52:32 -0800

    Added code first classes

---
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FakebookPosts.DataModel
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string Picture { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
