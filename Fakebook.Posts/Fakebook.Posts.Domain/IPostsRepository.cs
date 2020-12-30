33926ae38ebb15164da8c3fec904cbc867c46d3a
harelfishbein <harelfishbein@github.com>
2020-12-30 12:08:38 -0800

    changed call signature of addAsync for async

--- Fakebook.Posts/Fakebook.Posts.Domain/IPostsRepository.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/IPostsRepository.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Fakebook.Posts.Domain {
     public interface IPostsRepository : ICollection<Post>, IAsyncEnumerable<Post>
     {
-        public void AddAsync(Post post);
-        // public void RemoveAsync(Post post);
+        public ValueTask<bool> AddAsync(Post post);
+        // public ValueTask<bool> RemoveAsync(Post post);
     }
 }
