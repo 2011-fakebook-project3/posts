b98c743245b0b15a0ba733569655c4b611226b34
Matthew Goodman <mgoodman934@gmail.com>
2020-12-31 15:14:38 -0500

    add summary comments on controller tests

--- Fakebook.Posts/Fakebook.Posts.UnitTests/Controllers/PostControllerTest.cs
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/Controllers/PostControllerTest.cs
@@ -15,6 +15,9 @@ using Xunit;
 namespace Fakebook.Posts.UnitTests.Controllers {
     public class PostControllerTest {
 
+        /// <summary>
+        /// Tests the PostsController class' PostAsync method. Ensures that a proper Post object results in Status201Created.
+        /// </summary>
         [Fact]
         public async Task PostAsync_ValidPost_Creates() {
             // Arrange
@@ -52,6 +55,9 @@ namespace Fakebook.Posts.UnitTests.Controllers {
             Assert.Equal(StatusCodes.Status201Created, result.StatusCode);
         }
 
+        /// <summary>
+        /// Tests the PostsController class' PostAsync method. Ensures that an improper Post object results in Status400BadRequest with an error message in the body.
+        /// </summary>
         [Fact]
         public async Task PostAsync_InvalidPost_BadRequest() {
             // Arrange
