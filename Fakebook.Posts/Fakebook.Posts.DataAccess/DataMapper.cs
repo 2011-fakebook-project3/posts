27c3922839b5fad7ebb2859ed2725465e1e0a4ab
Alex Soto <alex.soto0624@gmail.com>
2020-12-31 12:08:56 -0800

    Added DataMapper Class

---
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/DataMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace FakebookPosts.DataModel
+{
+    /// <summary>
+    /// Mapper convert from data access layer models to domain models and back
+    /// intended to be called only on posts but can be called on comments
+    /// </summary>
+    public static class DataMapper
+    {
+        public static Fakebook.Posts.Domain.Models.Post ToDomain(this Post post)
+        {
+            throw new NotImplementedException();
+        }
+        public static Fakebook.Posts.Domain.Models.Comment ToDomain(this Comment comment,
+            Fakebook.Posts.Domain.Models.Post post)
+        {
+            throw new NotImplementedException();
+        }
+        public static Post ToDataAccess(this Fakebook.Posts.Domain.Models.Post post)
+        {
+            throw new NotImplementedException();
+        }
+        public static Comment ToDataAccess(this Fakebook.Posts.Domain.Models.Comment comment, Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
