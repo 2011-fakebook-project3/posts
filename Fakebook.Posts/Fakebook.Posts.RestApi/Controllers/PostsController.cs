1bb0db4ae42321f4f0db0d4e6904b2ed9726734f
Matthew Goodman <mgoodman934@gmail.com>
2021-01-04 10:15:24 -0500

    skeleton controller method and constructor

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Fakebook.Posts.Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,5 +10,16 @@ namespace Fakebook.Posts.RestApi.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class PostsController : ControllerBase {
+
+        public PostsController(/* IPostsRepository postsRepository */) {
+
+        }
+
+        // [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Post postModel) {
+            throw new NotImplementedException();
+        }
+
     }
 }
