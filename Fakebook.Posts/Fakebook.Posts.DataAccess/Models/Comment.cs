1c0891cea1af93bd6fcbfd686f73f7a16ca1594e
harelfishbein <harelfishbein@github.com>
2021-01-04 09:37:15 -0800

    very broken data access models

--- Fakebook.Posts/Fakebook.Posts.DataAccess/Models/Comment.cs
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/Models/Comment.cs
@@ -1,7 +1,38 @@
-﻿namespace Fakebook.Posts.DataAccess.Models
+﻿using System;
+
+namespace Fakebook.Posts.DataAccess.Models
 {
     public class Comment
     {
-
+        public int Id 
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public string UserEmail 
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public int PostId
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public Post Post
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public string Content
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public DateTime CreatedAt
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
     }
 }
--- Fakebook.Posts/Fakebook.Posts.DataAccess/Models/Post.cs
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/Models/Post.cs
@@ -1,7 +1,41 @@
-﻿namespace Fakebook.Posts.DataAccess.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fakebook.Posts.DataAccess.Models
 {
     public class Post
     {
-
+        public int Id
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public string UserEmail
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public string Content
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        public string Picture
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        
+        public DateTime CreatedAt
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
+        
+        public virtual ICollection<Comment> Comments
+        {
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
+        }
     }
 }
