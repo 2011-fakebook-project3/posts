c566e2189f524f64f0f47b8ab45c3dc1cc3ec35b
Paul Cortez <cortezpaul19@gmail.com>
2021-01-04 10:33:18 -0600

    Fixed Bugs for Domain Model Tests

--- Fakebook.Posts/Fakebook.Posts.UnitTests/Domain.Model.Test/Comment.cs
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/Domain.Model.Test/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fakebook.Posts.Domain;
+using System;
 using Xunit;
 
 namespace Fakebook.Posts.UnitTests.Model_Test
--- Fakebook.Posts/Fakebook.Posts.UnitTests/Domain.Model.Test/Post.cs
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/Domain.Model.Test/Post.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fakebook.Posts.Domain;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
