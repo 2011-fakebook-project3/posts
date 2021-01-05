7cd2436d326754a20c8bd63f0d3502f8bf9c6044
Paul Cortez <cortezpaul19@gmail.com>
2021-01-05 16:35:53 -0600

    Added Domain Models

--- Fakebook.Posts/Fakebook.Posts.Domain/Models/Comment.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/Models/Comment.cs
@@ -4,14 +4,16 @@ namespace Fakebook.Posts.Domain.Models
 {
     public class Comment
     {
-        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Content { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Post Post { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string UserEmail { get => throw new NotImplementedException(); private set => throw new NotImplementedException(); }
-        public DateTime CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public Post Post { get; set; }
+        public string UserEmail { get; private set; }
+        public DateTime CreatedAt { get; set; }
 
-        public Comment(string userEmail) {
-            if (string.IsNullOrWhiteSpace(userEmail)) {
+        public Comment(string userEmail)
+        {
+            if (string.IsNullOrWhiteSpace(userEmail))
+            {
                 throw new ArgumentException("Email cannot be null or whitespace.", nameof(userEmail));
             }
             UserEmail = userEmail;
