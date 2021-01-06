79763eb8c1655c66444f64698a8d3ab7d2a6f468
Matthew Goodman <mgoodman934@gmail.com>
2021-01-06 15:23:26 -0500

    Update Dbcontext - Specify schema for tables - Add required properties for UserEmail and Content - Change Picture property to be nullable, remove ValueGeneratedOnAddOrUpdate - Change DataAccess models to use DateTimeOffset instead of DateTime - Remove OnModelCreatingPartial method

--- Fakebook.Posts/Fakebook.Posts.DataAccess/FakebookPostsContext.cs
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/FakebookPostsContext.cs
@@ -9,19 +9,48 @@ namespace Fakebook.Posts.DataAccess {
         public virtual DbSet<Post> Posts { get; set; }
         public virtual DbSet<Comment> Comments { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
             modelBuilder.Entity<Post>(entity => {
-                entity.ToTable("Post");
-                entity.Property(e => e.CreatedAt).HasDefaultValueSql("NOW()").ValueGeneratedOnAdd();
-                entity.Property(e => e.Picture).HasDefaultValue("").ValueGeneratedOnAddOrUpdate();
-            });
-            modelBuilder.Entity<Comment>(entity => {
-                entity.ToTable("Comment");
-                entity.Property(e => e.CreatedAt).HasDefaultValueSql("NOW()").ValueGeneratedOnAdd();
-                entity.HasOne(d => d.Post).WithMany(p => p.Comments).OnDelete(DeleteBehavior.Cascade);
+
+                entity.ToTable("Post", "Fakebook");
+
+                entity.Property(e => e.UserEmail)
+                      .IsRequired();
+
+                entity.Property(e => e.Content)
+                      .IsRequired();
+
+                entity.Property(e => e.CreatedAt)
+                      .HasColumnType("time with time zone")
+                      .HasDefaultValueSql("NOW()")
+                      .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Picture)
+                      .IsRequired(false);
             });
 
-            OnModelCreatingPartial(modelBuilder);
+            modelBuilder.Entity<Comment>(entity => {
+
+                entity.ToTable("Comment", "Fakebook");
+
+                entity.Property(e => e.UserEmail)
+                      .IsRequired();
+
+                entity.Property(e => e.Content)
+                      .IsRequired();
+
+                entity.Property(e => e.CreatedAt)
+                      .HasColumnType("time with time zone")
+                      .HasDefaultValueSql("NOW()")
+                      .ValueGeneratedOnAdd();
+
+                entity.HasOne(e => e.Post)
+                      .WithMany(e => e.Comments)
+                      .HasForeignKey(e => e.PostId)
+                      .HasConstraintName("FK_Comment_Post")
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
         }
-        partial void OnModelCreatingPartial(ModelBuilder builder);
     }
 }
