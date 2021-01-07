8101c02335e3dddf10f1be32fab9cdf664a06f1e
harelfishbein <harelfishbein@github.com>
2021-01-07 13:49:44 -0800

    fix using statements

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
@@ -1,14 +1,12 @@
-﻿﻿using Fakebook.Posts.Domain.Models;
-using Fakebook.Posts.Domain;
+﻿﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Fakebook.Posts.Domain.Interfaces;
+using Fakebook.Posts.Domain.Models;
 
 namespace Fakebook.Posts.RestApi.Controllers {
     [Route("api/[controller]")]
