417a22de3262a6ec55883736973b0fbaf774b5ee
harelfishbein <harelfishbein@github.com>
2021-01-07 10:36:48 -0800

    implement get by id

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Fakebook.Posts.Domain.Interfaces;
 
 namespace Fakebook.Posts.RestApi.Controllers {
     [Route("api/[controller]")]
@@ -40,7 +41,11 @@ namespace Fakebook.Posts.RestApi.Controllers {
         [HttpGet("{id}")]
         [ActionName(nameof(GetAsync))]
         public async Task<IActionResult> GetAsync(int id) {
-            throw new NotImplementedException();
+            if (await _postsRepository.AsQueryable().FirstOrDefaultAsync(x => x.Id == id) is Post post)
+            {
+                return Ok(post);
+            }
+            return NotFound();
         }
     }
 }
