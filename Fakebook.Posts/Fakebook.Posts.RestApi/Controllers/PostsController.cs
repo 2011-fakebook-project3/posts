94ead458a1cf1b2338c65aa43d91390e1b46df81
Matthew Goodman <mgoodman934@gmail.com>
2021-01-07 17:48:43 -0500

    Implement PostsController PostAsync method.

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/PostsController.cs
@@ -1,6 +1,9 @@
 ﻿using Fakebook.Posts.Domain;
 using Fakebook.Posts.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -11,16 +14,43 @@ namespace Fakebook.Posts.RestApi.Controllers
     public class PostsController : ControllerBase
     {
 
-        public PostsController(IPostsRepository postsRepository)
-        {
-            throw new NotImplementedException();
+        private readonly IPostsRepository _postsRepository;
+        private readonly ILogger<PostsController> _logger;
+
+        public PostsController(IPostsRepository postsRepository, ILogger<PostsController> logger) {
+            _postsRepository = postsRepository;
+            _logger = logger;
         }
 
+        /// <summary>
+        /// Adds a new post to the database.
+        /// </summary>
+        /// <param name="postModel">The post object to be added.</param>
+        /// <returns>201Created on successful add, 400BadRequest on failure, 403Forbidden if post UserEmail does not match the email of the session token.</returns>
+        [Authorize]
         [HttpPost]
-        public async Task<IActionResult> PostAsync(Post postModel)
-        {
-            throw new NotImplementedException();
+        public async Task<IActionResult> PostAsync(Post postModel) {
+            var email = User.FindFirst(ct => ct.Type.Contains("nameidentifier")).Value; // Get user email from session.
+
+            if (email != postModel.UserEmail) return Forbid();
+
+            Post created;
+            try {
+                created = await _postsRepository.AddAsync(postModel);
+            } catch (ArgumentException e) {
+                _logger.LogInformation(e, "Attempted to create a post with invalid arguments.");
+                return BadRequest(e.Message);
+            } catch (DbUpdateException e) {
+                _logger.LogInformation(e, "Attempted to create a post which violated database constraints.");
+                return BadRequest(e.Message);
+            }
+
+            return CreatedAtAction(nameof(GetAsync), new { id = created.Id }, created);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id) {
+            throw new NotImplementedException();
+        }
     }
 }
