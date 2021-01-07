64626b36f68e2c455a1f2889321ddbcb4ab869d4
Alex Soto <alex.soto0624@gmail.com>
2021-01-07 09:38:50 -0800

    Added Repository Interface, Class and Test for create post method

---
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/Repositories/PostsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Fakebook.Posts.Domain.Models;
+using Fakebook.Posts.Domain.Interfaces;
+
+namespace Fakebook.Posts.DataAccess.Repositories
+{
+    public class PostsRepository : IPostsRepository
+    {
+        private readonly FakebookPostsContext _context;
+        public PostsRepository(FakebookPostsContext context)
+        {
+            _context = context;
+        }
+        public async ValueTask<Fakebook.Posts.Domain.Models.Post> AddAsync(Fakebook.Posts.Domain.Models.Post post)
+        {
+            var postDb = post.ToDataAccess();
+            await _context.Posts.AddAsync(postDb);
+            await _context.SaveChangesAsync();
+            return postDb.ToDomain();
+        }
+    }
+}
---
+++ Fakebook.Posts/Fakebook.Posts.Domain/Interfaces/IPostsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Fakebook.Posts.Domain.Models;
+
+namespace Fakebook.Posts.Domain.Interfaces
+{
+    public interface IPostsRepository 
+    {
+        ValueTask<Post> AddAsync(Post post);
+    }
+}
