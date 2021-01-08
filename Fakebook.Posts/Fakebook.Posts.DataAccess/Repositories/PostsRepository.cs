de594abf91e48aedb36a9516cb90a1ef8e72038a
harelfishbein <harelfishbein@github.com>
2021-01-08 14:49:49 -0800

    most like and unlike repo methods

--- Fakebook.Posts/Fakebook.Posts.DataAccess/Repositories/PostsRepository.cs
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/Repositories/PostsRepository.cs
@@ -1,11 +1,13 @@
-﻿using Fakebook.Posts.DataAccess.Mappers;
-using Fakebook.Posts.Domain.Interfaces;
-using Fakebook.Posts.Domain.Models;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Fakebook.Posts.DataAccess.Mappers;
+using Fakebook.Posts.Domain.Interfaces;
+//using Fakebook.Posts.DataAccess.Models;
+using Fakebook.Posts.Domain.Models;
 
 namespace Fakebook.Posts.DataAccess.Repositories
 {
@@ -63,6 +65,68 @@ namespace Fakebook.Posts.DataAccess.Repositories
         }
 
         public void Add(Post item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> LikePost(int postId, string userEmail)
+        {
+            try {
+                await _context.PostLikes.AddAsync(new Models.PostLike { PostId = postId, LikerEmail = userEmail });
+                await _context.SaveChangesAsync();
+                return true;
+            } catch {
+                return false;
+            }
+        }
+
+        public async Task<bool> UnlikePost(int postId, string userEmail)
+        {
+            if (await _context.PostLikes.FindAsync(userEmail, postId) is Models.PostLike like)
+            {
+                _context.PostLikes.Remove(like);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public Task<bool> LikeComment(Domain.Models.Comment comment, string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UnlikeComment(Domain.Models.Comment comment, string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncEnumerator<Domain.Models.Post> IAsyncEnumerable<Domain.Models.Post>.GetAsyncEnumerator(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Domain.Models.Post item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Domain.Models.Post item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Domain.Models.Post[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Domain.Models.Post item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<Domain.Models.Post> IEnumerable<Domain.Models.Post>.GetEnumerator()
         {
             throw new NotImplementedException();
         }
--- Fakebook.Posts/Fakebook.Posts.Domain/Interfaces/IPostsRepository.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/Interfaces/IPostsRepository.cs
@@ -8,6 +8,10 @@ namespace Fakebook.Posts.Domain.Interfaces
     public interface IPostsRepository : IAsyncEnumerable<Post>, ICollection<Post>
     {
         ValueTask<Post> AddAsync(Post post);
+        Task<bool> LikePost(int postId, string userEmail);
+        Task<bool> UnlikePost(int postId, string userEmail);
+        Task<bool> LikeComment(Comment comment, string userEmail);
+        Task<bool> UnlikeComment(Comment comment, string userEmail);
 
     }
 }
