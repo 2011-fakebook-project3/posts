ce8560d090a2022624849af41f54cc2e9f005223
harelfishbein <harelfishbein@github.com>
2021-01-08 13:49:44 -0800

    added like models

---
+++ Fakebook.Posts/Fakebook.Posts.DataAccess/Models/CommentLike.cs
@@ -0,0 +1,9 @@
+namespace Fakebook.Posts.DataAccess.Models
+{
+    public class CommentLike
+    {
+        public string LikerEmail { get; set; }
+        // public int CommentId { get; set; }
+        public Comment Comment { get;  set; }
+    }
+}
