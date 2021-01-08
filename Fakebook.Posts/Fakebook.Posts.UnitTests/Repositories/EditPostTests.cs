1649346d3fe966223a8a3b277525e725d048306b
Paul Cortez <cortezpaul19@gmail.com>
2021-01-08 15:39:58 -0600

    Added test for edit post

--- Fakebook.Posts/Fakebook.Posts.UnitTests/Repositories/EditPostTests.cs
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/Repositories/EditPostTests.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-using Moq;
+﻿using Fakebook.Posts.DataAccess;
+using Fakebook.Posts.DataAccess.Repositories;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Fakebook.Posts.DataAccess;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
-namespace Fakebook.Posts.UnitTests.Repositories {
-    public class EditPostTests {
+namespace Fakebook.Posts.UnitTests.Repositories
+{
+    public class EditPostTests
+    {
 
         /// <summary>
         /// Tests the PostsRepository class' UpdateAsync method. Ensures that a proper Post object results in the database being modified.
         /// </summary>
         [Fact]
-        public async Task UpdateAsync_ValidPost_UpdatesDb() {
+        public async Task UpdateAsync_ValidPost_UpdatesDb()
+        {
 
             // Arrange
             using var connection = new SqliteConnection("Data Source=:memory:");
@@ -25,6 +25,36 @@ namespace Fakebook.Posts.UnitTests.Repositories {
             var options = new DbContextOptionsBuilder<FakebookPostsContext>()
                 .UseSqlite(connection)
                 .Options;
+            Domain.Models.Post post = new Domain.Models.Post("person@domain.net", "content")
+            {
+                Id = 1,
+                Content = "New Content",
+                CreatedAt = DateTime.Now
+            };
+            DataAccess.Models.Post _post = new DataAccess.Models.Post()
+            {
+                Id = 1,
+                UserEmail = "person@domain.net",
+                Content = "New Content",
+                CreatedAt = DateTime.Now
+            };
+            //Act
+
+            using var context = new FakebookPostsContext(options);
+
+            context.Database.EnsureCreated();
+            context.Posts.Add(_post);
+            context.SaveChanges();
+            post.Content = "updateContent";
+            var repo = new PostsRepository(context);
+            await repo.UpdateAsync(post);
+            var result = await context.Posts.FirstAsync(p => p.UserEmail == "person@domain.net");
+
+
+            Assert.True(result.Content == post.Content);
+            Assert.True(result.UserEmail == post.UserEmail);
+            Assert.True(result.CreatedAt == post.CreatedAt);
+
         }
     }
 }
