663cddd4bd606402d98ce05605159973f3cde6ac
nodeJayS <shinjayw@gmail.com>
2021-01-12 21:41:58 -0500

    Removed duplicate methods

--- Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/CommentsController.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Controllers/CommentsController.cs
@@ -96,43 +96,5 @@ namespace Fakebook.Posts.RestApi.Controllers {
             }
             return NoContent();
         }
-        
-        /// Add a new comment to the database. 
-        /// </summary>
-        /// <param name="comment">
-        /// A domain comment. 
-        /// </param>
-        /// <returns>
-        /// The newly created comment
-        /// </returns>
-        [Authorize]
-        [HttpPost()]
-        public async Task<IActionResult> PostAsync(Comment comment) {
-            /*var email = User.FindFirst(ct => ct.Type.Contains("nameidentifier")).Value;
-
-            if (email != comment.UserEmail) {
-                _logger.LogInformation("Authenticated user email did not match user email of the post.");
-                return Forbid();
-            }*/
-
-            Comment created;
-            try {
-                created = await _postsRepository.AddCommentAsync(comment);
-            } catch (ArgumentException e) {
-                _logger.LogInformation(e, "Attempted to create a comment with invalid arguments.");
-                return BadRequest(e.Message);
-            } catch (DbUpdateException e) {
-                _logger.LogInformation(e, "Attempted to create a comment which violated database constraints.");
-                return BadRequest(e.Message);
-            }
-
-            return CreatedAtAction(nameof(GetAsync), new { id = created.Id }, created);
-        }
-
-        [HttpGet("{id}")]
-        [ActionName(nameof(GetAsync))]
-        public async Task<IActionResult> GetAsync(int id) {
-            throw new NotImplementedException();
-        }
     }
 }
