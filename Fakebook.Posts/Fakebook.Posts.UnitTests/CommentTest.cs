8d628011ff738a57509b172b31e25bc73bdd6218
damionsilver <drsilver@alumni.uncg.edu>
2021-04-01 19:34:54 -0400

    fixing changes for ModelTest pull request

--- Fakebook.Posts/Fakebook.Posts.UnitTests/CommentTest.cs
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/CommentTest.cs
@@ -8,82 +8,89 @@ using Xunit;
 namespace Fakebook.Posts.UnitTests
 {
 
-    public class CommentTest{
+    public class CommentTest
+    {
         private const int Id = 1;
         [Fact]
-        public void Comment_Constructor_Pass(){
+        public void Comment_Constructor_Pass()
+        {
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
             Comment newcomment = new Comment(userEmail, Content);
 
             //assert
-                Assert.NotNull(newcomment);
-        }
-
-
-           [Fact]
-        public void Comment_EmailIsNotNull(){
-            //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
-            //act
-            Comment newcomment = new Comment(userEmail, Content);
-
-            //assert
-                Assert.NotNull(newcomment.UserEmail);
+            Assert.NotNull(newcomment);
         }
 
 
         [Fact]
-        public void Comment_EmailEquality(){
-
-             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+        public void Comment_EmailIsNotNull()
+        {
+            //arrange
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
             Comment newcomment = new Comment(userEmail, Content);
 
             //assert
-                Assert.Equal(userEmail,newcomment.UserEmail);
+            Assert.NotNull(newcomment.UserEmail);
         }
 
 
-            [Fact]
-        public void Comment_ContentIsNotNull(){
+        [Fact]
+        public void Comment_EmailEquality()
+        {
+
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
             Comment newcomment = new Comment(userEmail, Content);
 
             //assert
-                Assert.NotNull(newcomment.Content);
+            Assert.Equal(userEmail, newcomment.UserEmail);
         }
 
-            [Fact]
-        public void Comment_EmailisFormattedCorrectly(){
+
+        [Fact]
+        public void Comment_ContentIsNotNull()
+        {
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
             Comment newcomment = new Comment(userEmail, Content);
 
             //assert
-                Assert.Contains("@", newcomment.UserEmail);
+            Assert.NotNull(newcomment.Content);
         }
 
-           public void Comment_EmailFormationThrowsException(){
+        [Fact]
+        public void Comment_EmailisFormattedCorrectly()
+        {
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silvertest.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
-           
-            
+            Comment newcomment = new Comment(userEmail, Content);
 
             //assert
-                Assert.Throws<ArgumentException>(() => new Comment(userEmail, Content));
+            Assert.Contains("@", newcomment.UserEmail);
+        }
+
+        public void Comment_EmailFormationThrowsException()
+        {
+            //arrange
+            const string Content = "This is some content";
+            const string userEmail = "damion.silvertest.com";
+            //act
+
+
+
+            //assert
+            Assert.Throws<ArgumentException>(() => new Comment(userEmail, Content));
 
         }
 
--- Fakebook.Posts/Fakebook.Posts.UnitTests/PostTests.cs
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/PostTests.cs
@@ -8,99 +8,86 @@ using Xunit;
 namespace Fakebook.Posts.UnitTests
 {
 
-    public class PostTests{
+    public class PostTests
+    {
         private const int Id = 1;
         [Fact]
-        public void Post_Constructor_Pass(){
+        public void Post_Constructor_Pass()
+        {
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
-            Post newpost = new Post(userEmail,Content);
+            Post newpost = new Post(userEmail, Content);
 
             //assert
-                Assert.NotNull(newpost);
-        }
-    
-
-         [Fact]
-        public void Post_EmailIsNotNull(){
-            //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
-            //act
-            Post newcomment = new Post(userEmail, Content);
-
-            //assert
-                Assert.NotNull(newcomment.UserEmail);
+            Assert.NotNull(newpost);
         }
 
 
         [Fact]
-        public void Post_EmailEquality(){
-
-             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
-            //act
-            Post newcomment = new Post(userEmail, Content);
-
-            //assert
-                Assert.Equal(userEmail,newcomment.UserEmail);
-        }
-
-
-            [Fact]
-        public void Post_ContentIsNotNull(){
+        public void Post_EmailIsNotNull()
+        {
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
             Post newcomment = new Post(userEmail, Content);
 
             //assert
-                Assert.NotNull(newcomment.Content);
+            Assert.NotNull(newcomment.UserEmail);
         }
 
-            [Fact]
-        public void Post_EmailisFormattedCorrectly(){
+
+        [Fact]
+        public void Post_EmailEquality()
+        {
+
             //arrange
-                const string Content = "This is some content";
-                const string userEmail = "damion.silver@test.com";
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
             //act
             Post newcomment = new Post(userEmail, Content);
 
             //assert
-                Assert.Contains("@", newcomment.UserEmail);
-        }
-
-           public void Post_EmailFormationThrowsException(){
-          
-                const string Content = "This is some content";
-                const string userEmail = "damion.silvertest.com";
-        
-                Assert.Throws<ArgumentException>(() => new Post(userEmail, Content));
-
+            Assert.Equal(userEmail, newcomment.UserEmail);
         }
 
 
-        
+        [Fact]
+        public void Post_ContentIsNotNull()
+        {
+            //arrange
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
+            //act
+            Post newcomment = new Post(userEmail, Content);
 
+            //assert
+            Assert.NotNull(newcomment.Content);
+        }
 
+        [Fact]
+        public void Post_EmailisFormattedCorrectly()
+        {
+            //arrange
+            const string Content = "This is some content";
+            const string userEmail = "damion.silver@test.com";
+            //act
+            Post newcomment = new Post(userEmail, Content);
 
+            //assert
+            Assert.Contains("@", newcomment.UserEmail);
+        }
 
+        public void Post_EmailFormationThrowsException()
+        {
 
+            const string Content = "This is some content";
+            const string userEmail = "damion.silvertest.com";
 
+            Assert.Throws<ArgumentException>(() => new Post(userEmail, Content));
 
-
-
-
-
-
-
-
-
-
-
+        }
     }
 }
