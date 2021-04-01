8b1a4dbfda995862c7492ab1e82395d2a3f1776e
Andrew Thiele <308butterfly@gmail.com>
2021-04-01 14:58:37 -0500

    normalize branch

---
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/Model.Tests/Comment.Tests.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Fakebook.Posts.DataAccess.Mappers;
+using Xunit;
+
+namespace Fakebook.Posts.Models.Comment.UnitTests
+{
+    public class Comment
+    {
+
+    }
+}
