4296b99221e836094520a218e2364d606a2689ef
damionsilver <drsilver@alumni.uncg.edu>
2021-04-01 18:18:44 -0400

    added unit testing for bussiness logic

--- Fakebook.Posts/Fakebook.Posts.Domain/Models/Post.cs
+++ Fakebook.Posts/Fakebook.Posts.Domain/Models/Post.cs
@@ -6,17 +6,47 @@ namespace Fakebook.Posts.Domain.Models
     public class Post
     {
         public int Id { get; set; }
-        public string Content { get; set; }
+        private string _Content; 
         public string Picture { get; set; }
-        public string UserEmail { get; private set; }
+         private string _UserEmail;
         public DateTime CreatedAt { get; set; }
         public ICollection<Comment> Comments { get; set; }
         public ICollection<string> Likes { get; set; }
 
+
+         public string UserEmail{
+            get{
+                return _UserEmail;
+            }
+            set{
+                if(value.Contains('@')){
+                    _UserEmail = value;
+                }
+                else{
+                    throw new ArgumentException("A correct email format is required.");
+                }
+            }
+
+        }
+
+
+        public string Content{
+            get{
+                return _Content;
+            }
+            set{
+                if(value.Length > 1)
+                {
+                    _Content = value;
+                }
+                else{
+                    throw new ArgumentException("Please enter a comment.");
+                }
+            }
+        }
+
         public Post(string userEmail, string content)
         {
-            if (string.IsNullOrWhiteSpace(userEmail)) throw new ArgumentException("User email is required.", nameof(userEmail));
-            if (string.IsNullOrWhiteSpace(content)) throw new ArgumentException("Post content is required.", nameof(content));
             UserEmail = userEmail;
             Content = content;
             Comments = new HashSet<Comment>();
---
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/CommentTest.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fakebook.Posts.Domain.Models;
+using Xunit;
+namespace Fakebook.Posts.UnitTests
+{
+
+    public class CommentTest{
+        private const int Id = 1;
+        [Fact]
+        public void Comment_Constructor_Pass(){
+            //arrange
+                const string Content = "This is some content";
+                const string userEmail = "damion.silver@test.com";
+            //act
+            Comment newcomment = new Comment(userEmail, Content);
+
+            //assert
+                Assert.NotNull(newcomment);
+        }
+
+
+           [Fact]
+        public void Comment_EmailIsNotNull(){
+            //arrange
+                const string Content = "This is some content";
+                const string userEmail = "damion.silver@test.com";
+            //act
+            Comment newcomment = new Comment(userEmail, Content);
+
+            //assert
+                Assert.NotNull(newcomment.UserEmail);
+        }
+
+
+        [Fact]
+        public void Comment_EmailEquality(){
+
+             //arrange
+                const string Content = "This is some content";
+                const string userEmail = "damion.silver@test.com";
+            //act
+            Comment newcomment = new Comment(userEmail, Content);
+
+            //assert
+                Assert.Equal(userEmail,newcomment.UserEmail);
+        }
+
+
+            [Fact]
+        public void Comment_ContentIsNotNull(){
+            //arrange
+                const string Content = "This is some content";
+                const string userEmail = "damion.silver@test.com";
+            //act
+            Comment newcomment = new Comment(userEmail, Content);
+
+            //assert
+                Assert.NotNull(newcomment.Content);
+        }
+
+            [Fact]
+        public void Comment_EmailisFormattedCorrectly(){
+            //arrange
+                const string Content = "This is some content";
+                const string userEmail = "damion.silver@test.com";
+            //act
+            Comment newcomment = new Comment(userEmail, Content);
+
+            //assert
+                Assert.Contains("@", newcomment.UserEmail);
+        }
+
+           public void Comment_EmailFormationThrowsException(){
+            //arrange
+                const string Content = "This is some content";
+                const string userEmail = "damion.silvertest.com";
+            //act
+           
+            
+
+            //assert
+                Assert.Throws<ArgumentException>(() => new Comment(userEmail, Content));
+
+        }
+
+    }
+}
