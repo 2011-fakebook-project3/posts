c161f610ecf4eb7f8dea265a5ff0687ebbefcc99
Cole Samuelsen <csamuelsen@wisc.edu>
2021-04-02 11:01:01 -0500

    DTOs and Constants for use in integration testings

---
+++ Fakebook.Posts/Fakebook.Posts.RestApi/DTOs/EditPostDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Fakebook.Posts.Domain.Constants;
+
+namespace Fakebook.Posts.RestApi.DTOs
+{
+    public class EditPostDTO
+    {
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        [StringLength(Constants.PostMaxLength, ErrorMessage = "The post is longer than allowed", MinimumLength = Constants.MinimumLength)]
+        public string Content { get; set; }
+    }
+}
---
+++ Fakebook.Posts/Fakebook.Posts.RestApi/DTOs/NewPostDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Fakebook.Posts.Domain.Constants;
+
+namespace Fakebook.Posts.RestApi.DTOs
+{
+    public class NewPostDTO
+    {
+        [Required]
+        [StringLength(Constants.PostMaxLength, ErrorMessage = "New post is too long.", MinimumLength = Constants.MinimumLength)]
+        public string Content { get; set; }
+    }
+}
