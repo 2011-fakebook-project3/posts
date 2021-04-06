9176b7593dad42c2c7cd83a9ff016a1643ff154a
Bryson Ewell <brysonewell@outlook.com>
2021-04-06 15:20:43 -0400

    Made tests pass

--- Fakebook.Posts/Fakebook.Posts.RestApi/Services/NotificationService.cs
+++ Fakebook.Posts/Fakebook.Posts.RestApi/Services/NotificationService.cs
@@ -1,13 +1,21 @@
 ﻿using Fakebook.Posts.RestApi.DTOs;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Fakebook.Posts.RestApi.Services
 {
     public class NotificationService : INotificationService
     {
-        public NotificationService(HttpClient client) => throw new System.NotImplementedException();
+        private HttpClient _client;
 
-        public async Task<HttpResponseMessage> SendNotificationAsync(string endpoint, NotificationDTO notification) => throw new System.NotImplementedException();
+        public NotificationService(HttpClient client) => _client = client;
+
+        public async Task<HttpResponseMessage> SendNotificationAsync(string endpoint, NotificationDTO notification)
+        {
+            string jsonContent = JsonSerializer.Serialize(notification);
+            var response = await _client.PostAsync(endpoint, new StringContent(jsonContent));
+            return response;
+        }
     }
 }
