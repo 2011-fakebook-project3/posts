8c5dcdf99f0919f64f3646c40d20cbb839346dbe
damionsilver <drsilver@alumni.uncg.edu>
2021-04-15 12:09:32 -0400

    added testing for NewsFeedDto

---
+++ Fakebook.Posts/Fakebook.Posts.UnitTests/NewsFeedDtoTest.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Fakebook.Posts.RestApi.Dtos;
+using Xunit;
+
+namespace Fakebook.Posts.UnitTests
+{
+    public class NewsFeedDtoTest
+    {
+        private const int Id = 1;
+
+        [Fact]
+        public void NewsFeedDto_ValidatingEmails_ReturnsTrue()
+        {
+
+
+            //arrange
+            NewsFeedDto dto = new NewsFeedDto();
+            dto.Emails = new List<string> { "damion.silver@test.com" };
+            List<string> mockEmailDtoList = new List<string> { "damion.silver@test.com" };
+            //act
+            Assert.Equal(mockEmailDtoList, dto.Emails);
+
+        }
+
+    }
+}
