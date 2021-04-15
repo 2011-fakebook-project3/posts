12d41aa850dc59f3bcea40195e52f5938af0822b
damionsilver <drsilver@alumni.uncg.edu>
2021-04-15 11:48:10 -0400

    added test to ensure null exception wasn't getting thrown.

--- Fakebook.Posts/Fakebook.Posts.IntegrationTests/PostRepository.Test/NewsFeedTest.cs
+++ Fakebook.Posts/Fakebook.Posts.IntegrationTests/PostRepository.Test/NewsFeedTest.cs
@@ -63,5 +63,59 @@ namespace Fakebook.Posts.IntegrationTests.PostRepository.Test
                 Assert.Equal(2, count);
             }
         }
+
+
+
+
+        [Fact]
+        public async Task NewsFeedAsync_NullFollowerEmails_ThrowsArgumentNullExcpetion()
+        {
+            //Arrange
+            using SqliteConnection connection = new("Data Source=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<FakebookPostsContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            DataAccess.Models.Post dataAccessPost1 = new()
+            {
+                Id = 3,
+                UserEmail = "person@domain.net",
+                Content = "post content",
+                CreatedAt = DateTime.Now
+            };
+
+            DataAccess.Models.Post dataAccessPost2 = new()
+            {
+                Id = 4,
+                UserEmail = "person2@domain.net",
+                Content = "post content",
+                CreatedAt = DateTime.Now
+            };
+
+            DataAccess.Models.Post dataAccessPost3 = new()
+            {
+                Id = 5,
+                UserEmail = "person3@domain.net",
+                Content = "post content",
+                CreatedAt = DateTime.Now
+            };
+
+            //Act
+
+            using (FakebookPostsContext context = new(options))
+            {
+                context.Database.EnsureCreated();
+                PostsRepository repo = new(context);
+                context.Posts.Add(dataAccessPost1);
+                context.Posts.Add(dataAccessPost2);
+                context.Posts.Add(dataAccessPost3);
+                context.SaveChanges();
+
+                //Assert
+                await Assert.ThrowsAsync<ArgumentNullException>(() => repo.NewsfeedAsync(null));
+            }
+        }
     }
 }
